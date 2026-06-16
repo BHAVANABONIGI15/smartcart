bd129edc86b527fc3ff0d91cc45733e030e218c7
Bhavana <bonigibhavana@gmail.com>
2026-06-16 21:24:43 +0530

    Initial commit

---
+++ config.py
@@ -0,0 +1,25 @@
+# ------------------------------------------
+# config.py
+# ------------------------------------------
+
+SECRET_KEY = "your_secret_key"
+
+# MySQL Database
+DB_HOST = "localhost"
+DB_USER = "root"
+DB_PASSWORD = "Bhavana@15"
+DB_NAME = "smartcart_db"
+
+# Email SMTP Settings
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = 'anvikabonigi@gmail.com'
+MAIL_PASSWORD = 'aeii gkhj fgaj ybzj'
+
+# ------------------------------------------
+# Razorpay Settings
+# ------------------------------------------
+
+RAZORPAY_KEY_ID = "rzp_test_T0gG630b8BW0h0"
+RAZORPAY_KEY_SECRET = "3aJ34j6Z5ZcVE72vwpKMbqFT"
